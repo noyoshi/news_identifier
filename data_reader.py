dbc4455951ee62ea239d0ce19761e5ce7458655d
wtheisen <wtheisen@nd.edu>
2018-10-22 11:39:29 -0400

    Cosine similarity and baseline poggers

--- data_reader.py
+++ data_reader.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 import collections
 import csv
+import operator
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 from tqdm import tqdm
 
@@ -64,3 +68,60 @@ if __name__ == "__main__":
     print(articles)
     print(testing)
     print(training)
+
+    def get_vectors(*strings):
+        text = [t for t in strings]
+        vectorizer = CountVectorizer(text)
+        vectorizer.fit(text)
+        return vectorizer.transform(text).toarray()
+
+    def get_cosine_sim(*strings):
+        vectors = [t for t in get_vectors(*strings)]
+        return cosine_similarity(vectors)
+
+    sourceStringCombinedDict = collections.defaultdict(str)
+    for a in training:
+        sourceStringCombinedDict[a.source] += a.raw_content
+
+    correctGuesses = 0
+    totalGuesses = 0
+
+    for a in testing:
+        cosSimDict = {}
+        for source, sourceString in sourceStringCombinedDict.items():
+            cosSimDict[source] = get_cosine_sim(a.raw_content, sourceString)[0][1]
+
+        guess = max(cosSimDict.items(), key=operator.itemgetter(1))[0]
+        print('Source: ', a.source, ', Guess: ', guess)
+
+        if guess == a.source:
+            correctGuesses += 1
+        totalGuesses += 1
+        print('Correct Guess Percentage at', totalGuesses, ' articles studied: ', (correctGuesses / totalGuesses))
+
+    print('Correct Guess Percentage: ', (correctGuesses / totalGuesses))
+
+
+    # for source, string in sourceStringCombinedDict.items():
+    #     print(get_vector(string))
+
+
+    # sourceWordCountDict = collections.defaultdict(dict)
+    # count = 0
+    # for a in training:
+    #     for w in a._tokenize(a.raw_content):
+    #         if w not in sourceWordCountDict[a.source].keys():
+    #             sourceWordCountDict[a.source][w] = 1
+    #         else:
+    #             sourceWordCountDict[a.source][w] += 1
+
+    #     if count > 4:
+    #         break
+
+    # cleanedWordCountDict = collections.defaultdict(dict)
+    # for s in sourceWordCountDict.keys():
+    #     for w, c in sourceWordCountDict[s].items():
+    #         if c > 1:
+    #             cleanedWordCountDict[s][w] = c
+
+    # print(cleanedWordCountDict)
